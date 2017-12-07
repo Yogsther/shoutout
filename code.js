adef0ed784ba3248c195b3d9b393261982ff62f8
Olle Kaiser <Olle@Livingfor.it>
2017-12-07 20:32:08 +0100

    Generic commt

---
+++ code.js
@@ -0,0 +1,83 @@
+
+
+/*
+ --- Spawn hearts on hover (donation button) ---
+*/
+
+
+// Mouse status
+function mouseStatus(state){
+  mouseOver = state;
+  if(mouseOver){
+    spawnHearts();
+  }
+}
+
+
+
+var heartObject = new Image();
+heartObject.src = "img/heart.png";
+
+var activeHearts = []; // To prevent duplicated IDs
+
+function spawnHearts(){
+  // Spawn heart from Donate button
+  setTimeout(function(){
+    new heart();
+    }, 200);
+}
+
+
+
+function heart(){
+  
+  var thisHeart = heartObject;
+  var thisID = Math.floor(Math.random()*100000);
+  var thisTop = -14;
+  var thisRot = 0; // Rotation in degree (from 20 > -20)
+  var thisRotFlip = false; // Angle to roate
+  thisHeart.id = thisID;
+  thisHeart.style.opacity = 1;
+  thisHeart.style.top = "14em";
+
+  thisHeart.style.right = Math.floor(Math.random()*13)+1 + "em";
+  thisHeart.classList.add("heart");
+  document.getElementById("header_wrap").innerHTML += thisHeart.outerHTML;
+
+  heartLoop();
+  
+  function heartLoop(){
+    setTimeout(function (){
+    
+    document.getElementById(thisID).style.transform = "rotate(" + thisRot + "deg)";
+    document.getElementById(thisID).style.opacity -= 0.005
+    document.getElementById(thisID).style.top = thisTop + "em";
+    thisTop -= 0.05;
+    
+    var rotationSpeed = 2;
+    if(!thisRotFlip) thisRot += rotationSpeed;
+    if(thisRotFlip) thisRot -= rotationSpeed;
+      
+      
+    if(thisRot == 20){
+      thisRotFlip = true;
+    }
+    if(thisRot == -20){
+      thisRotFlip = false;
+    }
+    
+    
+    if(document.getElementById(thisID).style.opacity < 0){
+      return;
+    } else {
+      heartLoop();
+      }
+    }, 10);
+  }
+  
+  
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
